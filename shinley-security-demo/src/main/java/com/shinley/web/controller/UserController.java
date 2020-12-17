27a0371c2c17ca79ac92392aab3a66f8751bae8e
shinley <chenxl4@autoai.com>
2020-12-17 20:10:32 +0800

    添加单测

---
+++ shinley-security-demo/src/main/java/com/shinley/web/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.shinley.web.controller;
+
+import com.shinley.dto.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public List<User> query() {
+        List<User> users = new ArrayList<>();
+        users.add(new User());
+        users.add(new User());
+        users.add(new User());
+        return users;
+    }
+}
